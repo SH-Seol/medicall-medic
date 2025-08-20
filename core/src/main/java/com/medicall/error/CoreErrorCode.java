002d0d196a47f7ba92674769bff6f3654cc84970
Seunghwan Seol <tjftmd7@gmail.com>
2025-08-21 00:42:37 +0900

    feature: 에러 코드 추가

--- core/src/main/java/com/medicall/error/CoreErrorCode.java
+++ core/src/main/java/com/medicall/error/CoreErrorCode.java
@@ -1,6 +1,11 @@
 package com.medicall.error;
 
 public enum CoreErrorCode {
+    HOSPITAL001("HOSPITAL-001"),
+    PATIENT001("PATIENT-001"),
+    DOCTOR001("DOCTOR-001"),
+    DEPARTMENT001("DEPARTMENT-001"),
+    APPOINTMENT001("APPOINTMENT-001"),
     ;
     private final String code;
 
--- core/src/main/java/com/medicall/error/CoreErrorType.java
+++ core/src/main/java/com/medicall/error/CoreErrorType.java
@@ -1,15 +1,21 @@
 package com.medicall.error;
 
 public enum CoreErrorType {
+    HOSPITAL_NOT_FOUND(CoreErrorCode.HOSPITAL001, CoreErrorKind.NOT_FOUND, "존재하지 않는 병원입니다.", CoreErrorLevel.WARN),
+    PATIENT_NOT_FOUND(CoreErrorCode.HOSPITAL001, CoreErrorKind.NOT_FOUND, "존재하지 않는 환자입니다.", CoreErrorLevel.WARN),
+    DOCTOR_NOT_FOUND(CoreErrorCode.HOSPITAL001, CoreErrorKind.NOT_FOUND, "존재하지 않는 의사입니다.", CoreErrorLevel.WARN),
+    DEPARTMENT_NOT_FOUND(CoreErrorCode.DEPARTMENT001, CoreErrorKind.NOT_FOUND, "존재하지 않는 전공입니다.", CoreErrorLevel.WARN),
+    APPOINTMENT_NOT_FOUND(CoreErrorCode.APPOINTMENT001, CoreErrorKind.NOT_FOUND, "존재하지 않는 예약입니다.", CoreErrorLevel.WARN),
     ;
     private final CoreErrorCode errorCode;
     private final CoreErrorKind errorKind;
-    private String message;
+    private final String message;
     private final CoreErrorLevel errorLevel;
 
-    CoreErrorType(CoreErrorCode errorCode, CoreErrorKind errorKind, CoreErrorLevel errorLevel) {
+    CoreErrorType(CoreErrorCode errorCode, CoreErrorKind errorKind, String message, CoreErrorLevel errorLevel) {
         this.errorCode = errorCode;
         this.errorKind = errorKind;
+        this.message = message;
         this.errorLevel = errorLevel;
     }
 
